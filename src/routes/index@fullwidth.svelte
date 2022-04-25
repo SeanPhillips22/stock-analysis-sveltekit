a6c238f0adb20752d645c0677f0543058fff5ac9
Kris <kriistjanm@gmail.com>
2022-04-25 19:36:49 +0700

    Added some stuff to home page

--- src/routes/index@fullwidth.svelte
+++ src/routes/index@fullwidth.svelte
@@ -1,16 +1,24 @@
 <div class="hero">
-	<h1>Search for a stock to start your analysis</h1>
+	<div class="inner">
+		<h1>Search for a stock to start your analysis</h1>
 
-	<p>
-		Accurate information on 6000+ stocks, including all the companies in the S&P500 index. See stock prices, news,
-		financials, forecasts, charts and more.
-	</p>
+		<p class="tagline">
+			Accurate information on 6000+ stocks, including all the companies in the S&P500 index. See stock prices, news,
+			financials, forecasts, charts and more.
+		</p>
 
-	<h2>Links</h2>
-	<ul>
-		<li><a href="/stocks/">Stocks</a></li>
-		<li><a href="/stocks/aapl/">AAPL</a></li>
-	</ul>
+		<form action="/search/" method="get" role="search">
+			<div class="search-wrap">
+				<input class="search-input" type="text" placeholder="Company or stock symbol..." />
+			</div>
+		</form>
+
+		<h2>Links</h2>
+		<ul>
+			<li><a href="/stocks/">Stocks</a></li>
+			<li><a href="/stocks/aapl/">AAPL</a></li>
+		</ul>
+	</div>
 </div>
 
 <p>
@@ -74,11 +82,23 @@
 		@apply bg-gray-100 border-b border-gray-200 px-4 pb-12 pt-20 shadow-sm md:pt-32 md:pb-24 lg:pt-44 lg:pb-40 landscape:border-t-2 landscape:md:border-t-0;
 	}
 
-	h1 {
-		@apply text-3xl font-bold;
+	.inner {
+		@apply mx-auto max-w-[850px] text-center;
 	}
 
-	p {
-		@apply text-lg mb-4;
+	h1 {
+		@apply mb-3 text-2xl font-bold xs:text-3xl sm:mb-5 md:text-4xl lg:mb-7 lg:text-[42px];
+	}
+
+	.tagline {
+		@apply mb-4 text-base sm:text-lg md:mb-5 md:text-xl lg:mb-7 lg:text-[22px];
+	}
+
+	.search-wrap {
+		@apply relative mx-auto mb-5 flex max-w-[95%] items-center text-left md:max-w-[75%] lg:max-w-lg;
+	}
+
+	.search-input {
+		@apply grow rounded-sm border border-gray-200 py-1.5 pl-7 text-sm placeholder-gray-700 focus:border-gray-200 focus:shadow-lg focus:outline-none focus:ring-0 tiny:pl-8 xs:pl-10 xs:text-base md:py-2 w-full bg-gray-50 hover:bg-white focus:bg-white;
 	}
 </style>
