2adf09057d6fb5d879c060c937e97c30e9341155
kristjanmar <kriistjanm@gmail.com>
2022-05-11 14:45:41 +0700

    Disabled edge deploy, does not work

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,6 @@
-// import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
-import adapter from '@sveltejs/adapter-vercel'
+// import adapter from '@sveltejs/adapter-vercel'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -13,8 +13,8 @@ const config = {
 	],
 
 	kit: {
-		// adapter: adapter(),
-		adapter: adapter({ edge: true }),
+		adapter: adapter(),
+		// adapter: adapter({ edge: true }),
 		trailingSlash: 'always',
 		floc: true
 	},
